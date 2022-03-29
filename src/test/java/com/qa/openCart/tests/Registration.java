14f0a14745d4621e7ef833c64fe02bfd819f381f
Raviraj <ravirajqa2013@gmail.com>
2022-03-29 13:33:14 +0530

    added registration

---
+++ src/test/java/com/qa/openCart/tests/Registration.java
@@ -0,0 +1,11 @@
+package com.qa.openCart.tests;
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hii");
+	}
+
+}
