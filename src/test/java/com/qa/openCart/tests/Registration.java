9f567079ead4ae5ad7b44d9b03f183daa6ae272b
Raviraj <ravirajqa2013@gmail.com>
2022-03-29 14:00:12 +0530

    update registration

--- src/test/java/com/qa/openCart/tests/Registration.java
+++ src/test/java/com/qa/openCart/tests/Registration.java
@@ -2,10 +2,5 @@ package com.qa.openCart.tests;
 
 public class Registration {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hii");
-	}
-
+	
 }
